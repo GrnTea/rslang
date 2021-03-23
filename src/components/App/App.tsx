d0a9c38d0a58096c288c312068651b6e0fe034ab
Ekaterina Matveyeva <grinteabox@gmail.com>
2021-03-23 17:12:25 +0200

    feat: add Auth markup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,16 @@ import React from "react";
 
 import Footer from "../Footer";
 import Header from "../Header";
+import SignIn from "../Authorisation";
+// import {ThemeProvider} from "styled-components";
+
+
 
 const App = () => (
   <React.Fragment>
     <Header />
+      <SignIn/>
+
 
     <div>Hello World!</div>
     <Footer />
---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Menu} from "@material-ui/core";
+
+
+function MainPage() {
+  return (
+    <>
+    <header>MainPage</header>
+    <div>Application Desciption</div>
+    <div>Video</div>
+
+    </>
+
+  );
+}
+
+export default MainPage;
