eb0737dbffdbdcfff3495e93ccbf7af202e2c428
Ekaterina Matveyeva <grinteabox@gmail.com>
2021-03-25 18:52:38 +0200

    feat: add sign in sign up forms

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 import Footer from "../Footer";
 import Header from "../Header";
-import SignIn from "../Authorisation";
+import SignIn from "../Authorisation/SignIn";
+import SignUp from "../Authorisation/SignUp";
 // import {ThemeProvider} from "styled-components";
 
 
@@ -11,6 +12,7 @@ const App = () => (
   <React.Fragment>
     <Header />
       <SignIn/>
+      <SignUp/>
 
 
     <div>Hello World!</div>
--- src/components/Authorisation/SignIn/SignIn.tsx
+++ src/components/Authorisation/SignIn/SignIn.tsx
@@ -42,9 +42,9 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-     {/*   <Avatar className={classes.avatar}>
+        <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
-        </Avatar>*/}
+        </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
