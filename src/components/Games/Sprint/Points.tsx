184ca76a711357848b15b207ffa9b4bc955d98cc
ArseniyXaoc <xaoc190789@gmail.com>
2021-03-29 22:32:29 +0300

    feat: add icons

--- src/components/Games/Sprint/Points.tsx
+++ src/components/Games/Sprint/Points.tsx
@@ -1,11 +1,37 @@
 import React from "react";
-import SprintHeader from "./SprinInterface";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+import SvgIcon, { SvgIconProps } from "@material-ui/core/SvgIcon";
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    "& > svg": {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+function HomeIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
 
 const Points = ({ bonus } : {bonus: number}) => {
+  const classes = useStyles();
   const x = bonus;
   return (
-    <div className="sprint__bonus">
+    <div>
+      <div className={classes.root}>
+      <HomeIcon color="secondary" />
+      <HomeIcon style={{ color: green[500] }} />
+      <HomeIcon color="disabled" />
+    </div>
+      <div className="sprint__bonus">
       {bonus !== 0 && (<div> +{x} </div>)}
+    </div>
     </div>
   );
 };
