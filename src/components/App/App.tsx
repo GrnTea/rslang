d761847ed571b818b77e8b8f1f4c739260c1d06f
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-03-30 23:01:54 +0400

    merge with develop

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,12 +10,9 @@ import AudioCall from "../Games/AudioCall/AudioCallDescription";
 import Games from "../Games/Games";
 // import {ThemeProvider} from "styled-components";
 import SettingsPage from "../Pages/SettingsPage/SettingsPage";
-<<<<<<< HEAD
 import DictionaryPage from "../Pages/DictionaryPage/DictionaryPage";
-=======
 import SignInForm from "../Authorisation/SignInForm";
 import SignUpForm from "../Authorisation/SignUpForm";
->>>>>>> 8fc5621061a7c0a117b5b65ff18b5015171f91b6
 
 const App = () => (
   <React.Fragment>
