77e723ad207a8802290c1c1ca100aef2080978ac
Ekaterina Matveyeva <grinteabox@gmail.com>
2021-04-09 20:59:23 +0300

    feat: add fullscreen btn

--- src/components/Games/Hangman/HangmanDescription.tsx
+++ src/components/Games/Hangman/HangmanDescription.tsx
@@ -3,7 +3,7 @@ import GameMenu from "../GameMenu/GameMenu";
 
 export default function HangmanDescription() {
   const gameName = "виселица";
-  const description = "Тренировка Виселица развивает навык правописания слов. Чем больше слов ты знаешь, тем больше очков получишь.";
+  const description = "Тренировка Виселица развивает память и навык правописания слов. Чем больше слов ты знаешь, тем больше очков получишь.";
   return (
     <GameMenu gameName={gameName} description={description} />
   );
