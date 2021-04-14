0f7c48ee5e4daedae53fc992db7f1a963cd93041
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-04-14 22:27:21 +0400

    feat: update styles

--- src/components/Pages/DictionaryPage/DicrionaryPageStyles.tsx
+++ src/components/Pages/DictionaryPage/DicrionaryPageStyles.tsx
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 const dictionaryStyles = makeStyles({
   dictionaryContainer: {
     maxWidth: "1200px",
-    width: "1200px",
     margin: "0 auto",
     padding: "10px",
     display: "flex",
